16baf429ad639b9330e6ad2ddc6977384260a359
Samir Alaa <76245712+samiralaa@users.noreply.github.com>
2025-05-01 02:29:54 +0400

    test

--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://backendtest.blueboxpet.com'],
 
     'allowed_origins_patterns' => [],
 
